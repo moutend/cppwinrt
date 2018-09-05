56d9510c238674fae00c31153a5849e6e87986d7
Yoshiyuki Koyanagi <moutend@gmail.com>
2018-09-06 05:33:00 +0900

    Chore

--- Store/XamlButton/App.cpp
+++ Store/XamlButton/App.cpp
@@ -6,31 +6,23 @@ using namespace Windows::Foundation;
 using namespace Windows::UI::Xaml;
 using namespace Windows::UI::Xaml::Controls;
 
-struct App : ApplicationT<App>
-{
-    Button m_button{ nullptr };
-
-    void OnLaunched(LaunchActivatedEventArgs const &)
-    {
-        m_button = Button();
-        m_button.Content(box_value(L"Hello"));
-
-        m_button.Click([&](IInspectable const& sender, RoutedEventArgs const&)
-        {
-            Button button = sender.as<Button>();
-            WINRT_ASSERT(button == m_button);
-
-            hstring value = unbox_value<hstring>(button.Content());
-            button.Content(box_value(value + L" World"));
-        });
-
-        Window window = Window::Current();
-        window.Content(m_button);
-        window.Activate();
-    }
+struct App : ApplicationT<App> {
+  Button m_button{ nullptr };
+  void OnLaunched(LaunchActivatedEventArgs const &) {
+    m_button = Button();
+    m_button.Content(box_value(L"Hello"));
+    m_button.Click([&](IInspectable const& sender, RoutedEventArgs const&) {
+      Button button = sender.as<Button>();
+      WINRT_ASSERT(button == m_button);
+      hstring value = unbox_value<hstring>(button.Content());
+      button.Content(box_value(value + L" World"));
+    });
+    Window window = Window::Current();
+    window.Content(m_button);
+    window.Activate();
+  }
 };
 
-int __stdcall wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
-{
-    Application::Start([](auto &&) { make<App>(); });
+int __stdcall wWinMain(HINSTANCE, HINSTANCE, PWSTR, int) {
+  Application::Start([](auto &&) { make<App>(); });
 }
